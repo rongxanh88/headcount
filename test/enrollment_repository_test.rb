456dd4ff506ef6041c80f68681bf01660355e251
wthoma22 <w.thomas390@yahoo.com>
2017-04-10 16:19:06 -0600

    Add enrollment repo and district repo tests

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -3,9 +3,28 @@ require './lib/enrollment_repository'
 
 class EnrollmentRepositoryTest < Minitest::Test
 
+  attr_reader :file_name
+
+  def setup
+    @file_name = "./data/Kindergartners in full-day program.csv"
+  end
+
   def test_it_exists
     enrollment = EnrollmentRepository.new
     assert_instance_of EnrollmentRepository, enrollment
+  end
+
+  def test_load_data
+    enrollment_repository = EnrollmentRepository.new
+    assert enrollment_repository.load_data(file_name)
+  end
+
+  def test_find_name
+    enrollment_repository = EnrollmentRepository.new
+    data = enrollment_repository.load_data(file_name)
+    enrollment_name = "ACADEMY 20"
+    result = enrollment_repository.find_by_name(enrollment_name)
+    assert_instance_of Enrollment, result
   end
 
 end
