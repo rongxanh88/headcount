2fe020edea4cefead8a2fd500645773b8d271be2
wthoma22 <w.thomas390@yahoo.com>
2017-04-11 09:44:48 -0600

    small spelling change

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -18,7 +18,7 @@ class EnrollmentTest < Minitest::Test
     assert_equal ({2010=>0.391, 2011=>0.353, 2012=>0.267}), er.kindergarten_participation_by_year
   end
 
-  def test__participation_in_year
+  def test_participation_in_year
     er = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => '.391', 2011 => '.353', 2012 => '.267'}})
     assert_equal 0.391, er.kindergarten_participation_in_year(2010)
   end
