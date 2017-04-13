c0d04c85eaf58dc795ffd0e821865d386750b01c
wthoma22 <w.thomas390@yahoo.com>
2017-04-13 14:24:37 -0600

    Pass enrollment tests, add support for loading two files in enrollment class

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -13,7 +13,12 @@ class EnrollmentRepository
 
   def load_data(files)
     kindergarten_file = files[:enrollment][:kindergarten]
+    high_school_file = files[:enrollment][:high_school_graduation]
     @enrollments = Parser::Enrollments.get_data(kindergarten_file)
+
+    if !high_school_file.nil?
+      @enrollments = Parser::Enrollments.get_data(high_school_file)
+    end
   end
 
   def find_by_name(name)
