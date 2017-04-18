9848c51bbb273aa465e17c4c28b5f6fee3ed75ce
rongxanh88 <rongxanh88@gmail.com>
2017-04-18 09:03:49 -0600

    add method to get household income for year

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -13,4 +13,28 @@ class EconomicProfile
     @lunch_data = input[:math] || Hash.new
     @title_i_data = input[:reading] || Hash.new
   end
+
+  def median_household_income_in_year(year)
+    year_ranges = income_data.keys.map do |range|
+      (range[0]..range[1]).to_a
+    end
+
+    incomes_from_year = year_ranges.map do |year_range|
+      if year_range.include?(year)
+        key = [year_range.first, year_range.last]
+        income_data[key]
+      end
+    end
+
+    incomes_from_year.compact!
+    number_of_incomes = incomes_from_year.count
+    
+    raise UnknownDataError if number_of_incomes == 0
+
+    average_income = (incomes_from_year.reduce(:+)) / number_of_incomes
+  end
+end
+
+class UnknownDataError < Exception
+
 end
--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -2,9 +2,18 @@ require './lib/economic_profile_repository'
 require './test/test_helper'
 
 class EconomicProfileRepositoryTest < Minitest::Test
+  attr_reader :repo
 
   def setup
-
+    @repo = EconomicProfileRepository.new
+    repo.load_data({
+      :economic_profile => {
+        :median_household_income => "./data/Median household income.csv",
+        :children_in_poverty => "./data/School-aged children in poverty.csv",
+        :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+        :title_i => "./data/Title I students.csv"
+      }
+    })
   end
   
   def test_it_exists
@@ -12,6 +21,7 @@ class EconomicProfileRepositoryTest < Minitest::Test
   end
 
   def test_load_files
+    skip
     repo = EconomicProfileRepository.new
     repo.load_data({
       :economic_profile => {
@@ -21,5 +31,15 @@ class EconomicProfileRepositoryTest < Minitest::Test
         :title_i => "./data/Title I students.csv"
       }
     })
+  end
+
+  def test_median_household_income_for_year
+    district = repo.find_by_name("ACADEMY 20")
+    assert_equal 85060, district.median_household_income_in_year(2005)
+    assert_equal 87635, district.median_household_income_in_year(2009)
+
+    assert_raises UnknownDataError do
+      district.median_household_income_in_year(1900)
+    end
   end
 end
